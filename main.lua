d10deb9a1680e519051c7022b554dbd834094c6d
Daniel de Schiffart <danieldeschiffart@gmail.com>
2019-05-16 22:41:48 +0200

    Add raycaster class

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local Raycaster = require 'raycasting'
+
 local worldMap = {
 	{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
 	{ 1,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1 },
@@ -25,12 +27,12 @@ local worldMap = {
 	{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
 }
 
-local rays = {}
+local raycaster = Raycaster()
 
 local wW = love.graphics.getWidth()
 local wH = love.graphics.getHeight()
 
-local player = {}
+player = {}
 player.pos = {}
 player.dir = {}
 player.plane = {}
@@ -43,12 +45,12 @@ player.plane.y = 0
 player.plane.angle = 0
 player.theta = math.pi / 2
 
-local camera = {}
+camera = {}
 camera.fov = 90
 camera.skew = 0
 camera.stretch = 0
 
-local var = 1
+var = 1
 local gamma = 0
 
 function love.load()
@@ -100,134 +102,11 @@ function love.update(dt)
 	player.plane.x = var * math.sin(player.theta + camera.skew)
 	player.plane.y = var * -math.cos(player.theta + camera.skew)
 
-	-- Preparing for main raycasting loop
-	local pos = {} -- Player position
-	local dir = {} -- Player direction vector
-	local plane = {} -- Camera plane vector from direction vector
-	pos.x = player.pos.x
-	pos.y = player.pos.y
-	dir.x = player.dir.x
-	dir.y = player.dir.y
-	plane.x = player.plane.x
-	plane.y = player.plane.y
-
-	rays = {}
-
-	-- Main raycasting loop
-	-- Runs loop once per vertical pixel line
-	for x = 0,wW do
-		local camera_x -- X row position in map
-		local hit = false -- Loop bool while it doesn't hit
-		local side -- Bool if it ray hits side of tile
-
-		-- Table containing main distances used through the loop
-		local dist = {}
-		dist.ray = {}
-		dist.delta = {}
-		dist.side = {}
-		dist.step = {}
-
-		camera_x = 2 * x / wW - 1
-		dist.ray.x = dir.x + plane.x * camera_x
-		dist.ray.y = dir.y + plane.y * camera_x
-
-		local map = {}
-		map.x = math.floor(pos.x)
-		map.y = math.floor(pos.y)
-
-		-- Distance travelled by ray for one X or one Y unit
-		dist.delta.x = math.abs(1 / dist.ray.x)
-		dist.delta.y = math.abs(1 / dist.ray.y)
-
-		if dist.ray.x < 0 then
-			dist.step.x = -1
-			dist.side.x = (pos.x - map.x) * dist.delta.x
-		else
-			dist.step.x = 1
-			dist.side.x = (map.x + 1 - pos.x) * dist.delta.x
-		end
-
-		if dist.ray.y < 0 then
-			dist.step.y = -1
-			dist.side.y = (pos.y - map.y) * dist.delta.y
-		else
-			dist.step.y = 1
-			dist.side.y = (map.y + 1 - pos.y) * dist.delta.y
-		end
-
-		-- Increase the ray distance if it hasn't hit a full tile yet
-		while not hit do
-			if dist.side.x < dist.side.y then
-				dist.side.x = dist.side.x + dist.delta.x
-				map.x = map.x + dist.step.x
-				side = false
-			else
-				dist.side.y = dist.side.y + dist.delta.y
-				map.y = map.y + dist.step.y
-				side = true
-			end
-
-			if worldMap[map.x][map.y] > 0 then
-				hit = true
-			end
-		end
-
-		-- Set the ray distance based on which side of the tile it hit
-		if not side then
-			dist.perpWall = (map.x - pos.x + (1 - dist.step.x) / 2) / dist.ray.x
-		else
-			dist.perpWall = (map.y - pos.y + (1 - dist.step.y) / 2) / dist.ray.y
-		end
-
-		-- Set on-screen line start and end positions based on ray distance
-		local lineHeight = wH * var / dist.perpWall
-		local drawStart = -lineHeight / 2 + wH / 2
-		local drawEnd = lineHeight / 2 + wH / 2
-
-		if drawStart < 0 then drawStart = 0 end
-		if drawEnd >= wH then drawEnd = wH - 1 end
-
-		-- Set color based on tile number
-		local tile = worldMap[map.x][map.y] 
-		local colorPalette = {
-			{0,1,0,1},
-			{0,0,1,1},
-			{1,0,0,1},
-			{1,1,0,1},
-		}
-
-		local tileColor = colorPalette[tile]
-
-		-- Add some shading depending on the side the ray hit
-		if side then
-			tileColor[4] = tileColor[4] / 2
-		end
-
-		--[[
-		if dist.perpWall < 20 then
-			tileColor[4] = tileColor[4] * ((10 / (dist.perpWall - 20)) + 1)
-		else
-			tileColor[4] = 0
-		end
-		--]]
-
-		-- Send back this vertical line's properties to be drawn in love.draw
-		local ray = {
-			x, drawStart, x, drawEnd,
-			tileColor = tileColor,
-		}
-
-		table.insert(rays, ray)
-	end
+	raycaster:update(dt, worldMap)
 end
 
 function love.draw()
-	-- Main "3D" drawing loop
-	love.graphics.setLineStyle('rough')
-	for _, ray in ipairs(rays) do
-		love.graphics.setColor(ray.tileColor)
-		love.graphics.line(ray)
-	end
+	raycaster:draw()
 
 	-- Minimap scale factor and variable importing
 	local factor = 10
---
+++ raycasting.lua
@@ -0,0 +1,143 @@
+local Class = require 'libs.hump.class'
+
+Raycaster = Class{}
+
+function Raycaster:init()
+	self.rays = {}
+end
+
+function Raycaster:update(dt, worldMap)
+	-- Preparing for main raycasting loop
+	local wW = love.graphics.getWidth()
+	local wH = love.graphics.getHeight()
+	local pos = {} -- Player position
+	local dir = {} -- Player direction vector
+	local plane = {} -- Camera plane vector from direction vector
+	pos.x = player.pos.x
+	pos.y = player.pos.y
+	dir.x = player.dir.x
+	dir.y = player.dir.y
+	plane.x = player.plane.x
+	plane.y = player.plane.y
+
+	self.rays = {}
+
+	-- Main raycasting loop
+	-- Runs loop once per vertical pixel line
+	for x = 0,wW do
+		local camera_x -- X row position in map
+		local hit = false -- Loop bool while it doesn't hit
+		local side -- Bool if it ray hits side of tile
+
+		-- Table containing main distances used through the loop
+		local dist = {}
+		dist.ray = {} -- Direction vector for cast ray
+		dist.delta = {} -- Vector of distance the ray covers each tile
+		dist.side = {} -- Vector to check which side of a tile the ray hits
+		dist.step = {} -- Vector of which 4-way direction to step in
+
+		camera_x = 2 * x / wW - 1
+		dist.ray.x = dir.x + plane.x * camera_x
+		dist.ray.y = dir.y + plane.y * camera_x
+
+		local map = {}
+		map.x = math.floor(pos.x)
+		map.y = math.floor(pos.y)
+
+		-- Distance travelled by ray for one X or one Y unit
+		dist.delta.x = math.abs(1 / dist.ray.x)
+		dist.delta.y = math.abs(1 / dist.ray.y)
+
+		if dist.ray.x < 0 then
+			dist.step.x = -1
+			dist.side.x = (pos.x - map.x) * dist.delta.x
+		else
+			dist.step.x = 1
+			dist.side.x = (map.x + 1 - pos.x) * dist.delta.x
+		end
+
+		if dist.ray.y < 0 then
+			dist.step.y = -1
+			dist.side.y = (pos.y - map.y) * dist.delta.y
+		else
+			dist.step.y = 1
+			dist.side.y = (map.y + 1 - pos.y) * dist.delta.y
+		end
+
+		-- Increase the ray distance if it hasn't hit a full tile yet
+		while not hit do
+			if dist.side.x < dist.side.y then
+				dist.side.x = dist.side.x + dist.delta.x
+				map.x = map.x + dist.step.x
+				side = false
+			else
+				dist.side.y = dist.side.y + dist.delta.y
+				map.y = map.y + dist.step.y
+				side = true
+			end
+
+			if worldMap[map.x][map.y] > 0 then
+				hit = true
+			end
+		end
+
+		-- Set the ray distance based on which side of the tile it hit
+		if not side then
+			dist.perpWall = (map.x - pos.x + (1 - dist.step.x) / 2) / dist.ray.x
+		else
+			dist.perpWall = (map.y - pos.y + (1 - dist.step.y) / 2) / dist.ray.y
+		end
+
+		-- Set on-screen line start and end positions based on ray distance
+		local lineHeight = wH * var / dist.perpWall
+		local drawStart = -lineHeight / 2 + wH / 2
+		local drawEnd = lineHeight / 2 + wH / 2
+
+		if drawStart < 0 then drawStart = 0 end
+		if drawEnd >= wH then drawEnd = wH - 1 end
+
+		-- Set color based on tile number
+		local tile = worldMap[map.x][map.y] 
+		local colorPalette = {
+			{0,1,0,1},
+			{0,0,1,1},
+			{1,0,0,1},
+			{1,1,0,1},
+		}
+
+		local tileColor = colorPalette[tile]
+
+		-- Add some shading depending on the side the ray hit
+		if side then
+			tileColor[4] = tileColor[4] / 2
+		end
+
+		--[[
+		if dist.perpWall < 20 then
+			tileColor[4] = tileColor[4] * ((10 / (dist.perpWall - 20)) + 1)
+		else
+			tileColor[4] = 0
+		end
+		--]]
+
+		-- Send back this vertical line's properties to be drawn in love.draw
+		local ray = {
+			x, drawStart, x, drawEnd,
+			tileColor = tileColor,
+		}
+
+		table.insert(self.rays, ray)
+	end
+end
+
+function Raycaster:draw()
+	-- Main "3D" drawing loop
+	love.graphics.setLineStyle('rough')
+	print(#self.rays)
+	for _, ray in ipairs(self.rays) do
+		love.graphics.setColor(ray.tileColor)
+		love.graphics.line(ray)
+	end
+end
+
+return Raycaster
